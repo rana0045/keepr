b2c5ff3ff5b2b5b4c0b8246a6601a544fb3043bf
rana0045 <ranaanees26@gmail.com>
2023-03-04 23:00:03 +0500

    last

---
+++ build/precache-manifest.e7a32ff4eaefad03274342991095ad12.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1728dcbd6fc58f0f481ead74c51f0dc1",
+    "url": "/notekeeper/index.html"
+  },
+  {
+    "revision": "d263d024e3cc7fe31726",
+    "url": "/notekeeper/static/js/2.d1237d8c.chunk.js"
+  },
+  {
+    "revision": "cd3fd94db4c57f0bad4a",
+    "url": "/notekeeper/static/js/main.6bca0a44.chunk.js"
+  },
+  {
+    "revision": "20b5a0d53eba9dd86ba8",
+    "url": "/notekeeper/static/js/runtime-main.f246ee32.js"
+  }
+]);
